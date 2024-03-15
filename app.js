a1c2c5161d6f1742da9fe5f89b6201f5382b2be1
mgt9613 <mgt9613@nyu.edu>
2024-03-14 20:39:54 -0400

    Full pipeline test with Snky Scan

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('App.js pipeline approval test'));
+app.get('/', (req, res) => res.send('App.js Snky Scan Test!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
